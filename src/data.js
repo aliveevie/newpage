7bbc0bb6ee2d8ba268f48a8db9a9b43c0bd2384e
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-06-17 12:49:48 +0100

    Initital Commit and Deploy

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Mini React Calucator",
+    subtitle: "React",
+    description:
+      "A simple React Calcutor was build",
+    image: "./computer-2500.gif",
+    link: "https://github.com/aliveevie/calculator",
+  },
+  {
+    title: "Exercise Tracker",
+    subtitle: "React",
+    description:
+      "An Exercise tracker was build to keep track of all the exercises",
+    image: "./exercise-4024_128.gif",
+    link: "https://github.com/aliveevie/boilerplate-project-exercisetracker",
+  },
+  {
+    title: "Issue Tracker",
+    subtitle: "Nodejs",
+    description:
+      "Tracking Issues",
+    image: "./lights-3470_128.gif",
+    link: "https://github.com/aliveevie/issue-tracker",
+  },
+  {
+    title: "Measurement Converter",
+    subtitle: "Nodejs",
+    description:
+      "Measurement Converter Units",
+    image: "./machinery-4004_128.gif",
+    link: "https://github.com/aliveevie/metric-imperial-converter",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
